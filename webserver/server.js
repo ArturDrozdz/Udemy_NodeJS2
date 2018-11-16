f7882153891fd16bca42f0c9cd0a5d5287324bfd
sullivan <sullivan.d42@gmail.com>
2018-11-16 14:18:58 +0100

    HBS Templating

--- webserver/server.js
+++ webserver/server.js
@@ -1,17 +1,50 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+/* __ MIDDLEWARE __ */
+app.use((req, res, next) => {
+    var now = new Date().toString();  
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log('Unable to append to server log file');
+    });
+    next();
+});
+
+/* __ HELPERS HBS __ */
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+/* __ ROUTING __ */
 app.get('/', (req, res) => {
-   // res.send('<h1> Hello Express ! </h1>');
-   res.send({
-       name: 'Sulivan',
-       age: '22'
-   })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to you, Sullivan',
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
-app.listen(3000);
+/* __ SERVER __ */
+app.listen(3000, () => {
+    console.log('Server is running on port 3000'); 
+});
